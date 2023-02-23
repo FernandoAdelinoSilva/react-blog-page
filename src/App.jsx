1b60b4d84e78cd949f976e7b3a41099951c0a827
Fernando Silva <fernando.a.silva@exxonmobil.com>
2023-02-23 17:13:09 -0300

    feat: create sidebar

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,22 @@
-import { Header } from './components/header';
+import { Header } from './components/Header';
+import { Sidebar } from './components/Sidebar';
 import { Post } from './Post';
 
+import styles from './App.module.css';
 import './global.css';
 
 export function App() {
   return (
     <>
       <Header />
-      <Post author="Fernando Silva" content="Text_1" />
-      <Post author="Fernando Silva" content="Text_2" />
+
+      <div className={styles.wrapper}>
+        <Sidebar />
+        <main>
+          <Post author="Fernando Silva" content="Text_1" />
+          <Post author="Fernando Silva" content="Text_2" />
+        </main>
+      </div>
     </>
   );
 }
