9b19860b8f690f334cd4ab0e976fa3010bcf0796
Fernando Silva <fernando.a.silva@exxonmobil.com>
2023-03-19 17:31:47 -0300

    feat: create styles for comments

---
+++ src/components/Comment/Comment.jsx
@@ -0,0 +1,36 @@
+import { ThumbsUp, Trash } from 'phosphor-react';
+import styles from './Comment.module.css';
+
+export function Comment() {
+  return (
+    <div className={styles.comment}>
+      <img
+        className={styles.avatar}
+        src="https://github.com/FernandoAdelinoSilva.png"
+      />
+
+      <div className={styles.commentBox}>
+        <div className={styles.commentContent}>
+          <header>
+            <div className={styles.authorAndTime}>
+              <strong>Fernando Silva</strong>
+              <time title="February 02" dateTime="2023-02-02 08:00:00">
+                1h ago
+              </time>
+            </div>
+            <button title="Delete Comment">
+              <Trash size={24} />
+            </button>
+          </header>
+          <p>Good Work</p>
+        </div>
+        <footer>
+          <button>
+            <ThumbsUp />
+            Like <span>20</span>
+          </button>
+        </footer>
+      </div>
+    </div>
+  );
+}
